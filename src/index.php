cceb5c070e576260a0dd4709dcb338dace248cc7
fruwei <fruwei12@gmail.com>
2016-10-15 22:11:39 +0800

    wx share

--- src/index.php
+++ src/index.php
@@ -1,5 +1,75 @@
+<?php
+$appId = 'wx8e34afcb70db689f';
+$appsecret = 'a9a38c91aa3bb4817a21569a8c8ae662';
+$timestamp = time();
+$jsapi_ticket = make_ticket($appId,$appsecret);
+$nonceStr = make_nonceStr();
+$url = 'http://rubbyjiang.daoapp.io/';
+$signature = make_signature($nonceStr,$timestamp,$jsapi_ticket,$url);
+function make_nonceStr()
+{
+	$codeSet = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	for ($i = 0; $i<16; $i++) {
+		$codes[$i] = $codeSet[mt_rand(0, strlen($codeSet)-1)];
+	}
+	$nonceStr = implode($codes);
+	return $nonceStr;
+}
+function make_signature($nonceStr,$timestamp,$jsapi_ticket,$url)
+{
+	$tmpArr = array(
+	'noncestr' => $nonceStr,
+	'timestamp' => $timestamp,
+	'jsapi_ticket' => $jsapi_ticket,
+	'url' => $url
+	);
+	ksort($tmpArr, SORT_STRING);
+	$string1 = http_build_query( $tmpArr );
+	$string1 = urldecode( $string1 );
+	$signature = sha1( $string1 );
+	return $signature;
+}
+function make_ticket($appId,$appsecret)
+{
+	// access_token 应该全局存储与更新，以下代码以写入到文件中做示例
+	$data = json_decode(file_get_contents("access_token.json"));
+	if ($data->expire_time < time()) {
+		$TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appId."&secret=".$appsecret;
+		$json = file_get_contents($TOKEN_URL);
+		$result = json_decode($json,true);
+		$access_token = $result['access_token'];
+		if ($access_token) {
+			$data->expire_time = time() + 7000;
+			$data->access_token = $access_token;
+			$fp = fopen("access_token.json", "w");
+			fwrite($fp, json_encode($data));
+			fclose($fp);
+		}
+	}else{
+		$access_token = $data->access_token;
+	}
+	// jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
+	$data = json_decode(file_get_contents("jsapi_ticket.json"));
+	if ($data->expire_time < time()) {
+		$ticket_URL="https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=".$access_token."&type=jsapi";
+		$json = file_get_contents($ticket_URL);
+		$result = json_decode($json,true);
+		$ticket = $result['ticket'];
+		if ($ticket) {
+			$data->expire_time = time() + 7000;
+			$data->jsapi_ticket = $ticket;
+			$fp = fopen("jsapi_ticket.json", "w");
+			fwrite($fp, json_encode($data));
+			fclose($fp);
+		}
+	}else{
+		$ticket = $data->jsapi_ticket;
+	}
+	return $ticket;
+}
+?>
 <!DOCTYPE html>
-	<html>
+<html>
 
 	<head>
 		<meta charset="utf-8">
@@ -14,6 +84,70 @@
 
 		<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
 		<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+		<script>
+			wx.config({
+				debug: false,
+				appId: '<?php echo $signPackage["appId"];?>',
+				timestamp: <?php echo $signPackage["timestamp"];?>,
+				nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+				signature: '<?php echo $signPackage["signature"];?>',
+				jsApiList: [
+					// 所有要调用的 API 都要加到这个列表中
+					'checkJsApi',
+					'onMenuShareTimeline',
+					'onMenuShareAppMessage'
+				]
+			});
+			wx.ready(function() {
+
+				wx.onMenuShareAppMessage({
+					title: '我要胡！',
+					desc: '答对了***题，看看你能超过我吗？',
+					link: 'http://rubbyjiang.daoapp.io/',
+					trigger: function(res) {
+						// 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+						// alert('用户点击发送给朋友');
+					},
+					success: function(res) {
+						// alert('已分享');
+					},
+					cancel: function(res) {
+						// alert('已取消');
+					},
+					fail: function(res) {
+						// alert(JSON.stringify(res));
+					}
+				});
+
+			});
+		</script>
+		<script>
+			wx.config({
+				debug: true,
+				appId: '<?=$appId?>',
+				timestamp: <?=$timestamp?>,
+				nonceStr: '<?=$nonceStr?>',
+				signature: '<?=$signature?>',
+				jsApiList: [
+					'checkJsApi',
+					'onMenuShareTimeline',
+					'onMenuShareAppMessage'
+				]
+			});
+			wx.ready(function() {
+				var shareData = {
+					title: '这里是分享标题',
+					desc: '这里是发送给好友的时候的简介',
+					link: 'http://baidu.com',
+					imgUrl: 'http://baidu.com/logo.jpg'
+				};
+				wx.onMenuShareAppMessage(shareData);
+				wx.onMenuShareTimeline(shareData);
+			});
+			wx.error(function(res) {
+				alert(res.errMsg);
+			});
+		</script>
 
 		<style>
 			.img_mj {
@@ -143,8 +277,9 @@
 				</div>
 			</div>
 			<div class="btn_ctl row">
-<!--				<a class="a_demo_one" id="btn_share">分享</a>
--->				<a class="a_demo_one" id="btn_help">帮助</a>
+				<!--				<a class="a_demo_one" id="btn_share">分享</a>
+-->
+				<a class="a_demo_one" id="btn_help">帮助</a>
 				<a class="a_demo_one" id="btn_ok">胡牌</a>
 
 			</div>
@@ -180,10 +315,10 @@
 						<h4 class="modal-title" id="myModalLabel">我要胡！</h4>
 					</div>
 					<div class="modal-body" style="text-align: left;">
-						<p>就差一张牌就胡啦!赶快来找出它吧! </p>
+						<p>您已经听牌啦！赶快来找出所有能胡的牌吧! </p>
 						<p>1.胡牌规则：一个对子(AA)+N个三个连牌(ABC)或三个同样的牌(AAA). </p>
 						<p>2.有三次答错机会. </p>
-						<p>3.选择可能会胡的牌，点击胡牌提交结果.</p>
+						<p>3.选择所有可能会胡的牌，点击胡牌提交结果.</p>
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">确定</button>
@@ -532,4 +667,4 @@
 		}
 	</script>
 
-	</html>
+</html>
